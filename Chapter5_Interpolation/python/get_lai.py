bca64c55ceb2e13c22628960f76680453081a81d
profLewis <dr.p.e.lewis@gmail.com>
2013-10-25 17:41:03 +0100

    updated from geogg122

--- Chapter5_Interpolation/python/get_lai.py
+++ Chapter5_Interpolation/python/get_lai.py
@@ -38,4 +38,88 @@ def get_lai(filename, \
 
     return odata
 
+# thats quite good, so put as a function:
+import numpy.ma as ma
+import numpy as np
+import sys
+sys.path.insert(0,'files/python')
+from get_lai import get_lai
+from raster_mask import raster_mask
 
+
+def read_lai(filelist,datadir='files/data',country=None):
+    '''
+    Read MODIS LAI data from a set of files
+    in the list filelist. Data assumed to be in
+    directory datadir.
+    
+    Parameters:
+    filelist : list of LAI files
+    
+    Options:
+    datadir  : data directory
+    country  : country name (in files/data/world.shp)
+    
+    Returns:
+    lai dictionary
+    '''
+    fields = {'LaiStdDev_1km':[],'Lai_1km':[]}
+    
+    # make a dictionary
+    lai = {'filenames':np.sort(filelist)}
+    lai.update(fields)
+    
+    # make a raster mask
+    # from the layer IRELAND in world.shp
+    filename = lai['filenames'][0]
+    file_template = 'HDF4_EOS:EOS_GRID:"%s":MOD_Grid_MOD15A2:%s'
+    file_spec = file_template%('files/data/%s'%filename,'Lai_1km')
+                            
+    if country:
+        mask = raster_mask(file_spec,\
+                       target_vector_file = "files/data/world.shp",\
+                       attribute_filter = "NAME = '%s'"%country)
+        all_mask = ~mask
+        coords = np.where(all_mask)
+        # find the min/max extent of these
+        cmin = np.min(coords,axis=1)
+        cmax = np.max(coords,axis=1)
+        mask = mask[cmin[0]:cmax[0]+1,cmin[1]:cmax[1]+1]
+
+    for f in np.sort(lai['filenames']):
+        # would be more efficient to just read
+        # the required area
+        this_lai = get_lai('files/data/%s'%f)
+        for layer in fields.keys():
+            if country:
+              this_lai[layer] = \
+		this_lai[layer][cmin[0]:cmax[0]+1,cmin[1]:cmax[1]+1]
+            lai[layer].append(this_lai[layer])
+    
+    # put the mask in
+    for layer in fields.keys():
+        # first convert the list to a masked array
+        masked = ma.array(lai[layer])
+        if country:
+            # then and it with the country mask
+            lai[layer] = ma.array(lai[layer],mask=(mask | masked.mask)) 
+        else:
+            lai[layer] = masked
+
+    if not country:            
+      # a little bit of code to extract just the part we want ...
+      # sum the mask to see where there are valid data
+      all_mask = (~lai[selected_layers[0]].mask).sum(axis=0)
+      mask = np.zeros_like(all_mask).astype(bool)
+      mask[all_mask>0] = True
+      # use np.where to get the r,c coordinates 
+      # of valid pixels
+      coords = np.where(all_mask)
+      # find the min/max extent of these
+      cmin = np.min(coords,axis=1)
+      cmax = np.max(coords,axis=1)
+      for layer in selected_layers[:-1]:
+        lai[layer] = lai[layer][:,cmin[0]:cmax[0]+1,cmin[1]:cmax[1]+1]
+    lai['offset'] = cmin
+    
+    return lai
