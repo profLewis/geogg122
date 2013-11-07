5749cd14cf2f8981876240df3ea41bb5cb01432f
profLewis <dr.p.e.lewis@gmail.com>
2013-11-07 12:00:14 +0000

    updated from geogg122

---
+++ Chapter7_ENSO/python/reader.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pylab as plt
+import numpy.ma as ma
+import sys,os
+import gdal
+from glob import glob
+# file list
+
+files = np.sort(glob('files/data/MOD14CMH.*.005.01.hdf'))
+
+year = np.array([f.split(os.sep)[-1].split('.')[1][:4] for f in files]).astype(int)
+month= np.array([f.split(os.sep)[-1].split('.')[1][4:] for f in files]).astype(int)
+
+def reader(hdf):
+  pattern = 'HDF4_SDS:UNKNOWN:"%s":1'%hdf
+  g = gdal.Open(pattern)
+  d = g.ReadAsArray()
+  # invalid is -1
+  mask = (d == -1)
+  d[mask] = 0.0
+  return d,mask
+
+mask = reader(files[0])[1]
+data = [reader(f)[0] for f in files]
+masks = np.array([mask]*len(data))
+
+data = ma.array(data,mask=masks)
+
